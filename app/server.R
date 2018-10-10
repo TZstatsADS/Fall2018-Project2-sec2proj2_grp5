698f74104197dea3f2398309830d56ea58b4b82f
Shaolong Lin <shaolonglin09@gmail.com>
2018-10-10 11:25:05 -0400

    Update app/server.R

--- app/server.R
+++ app/server.R
@@ -6,9 +6,20 @@ library(leaflet)
 library(maps)
 library(rgdal)
 
+api_key <- 'AIzaSyAJZcM_Y6wM6z1MEGebLPnQCVHE8RpM3Qg'
+
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   
+  
+  output$travelPlanner <- renderLeaflet({
+    
+    leaflet() %>% 
+      setView(lat = 40.749, lng = -73.98, zoom = 12) %>%
+      addTiles()
+    
+  })
   
 })
