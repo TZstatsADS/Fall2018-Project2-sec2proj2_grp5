a0cb72fb9cfb39d00ab9fe633a108ff467888781
Sam Kolins <sk3651@columbia.edu>
2018-10-11 01:31:58 -0400

    Commenting out output$Netgraph for a second

--- app/server.R
+++ app/server.R
@@ -190,34 +190,34 @@ shinyServer(function(input, output) {
   ## Network Graph
   ################################################################
   
-  output$Netgraph <- renderLeaflet({
-    # remove trips whose station ID's are null
-    null.start <- citibikes$start.station.name == "NULL"
-    null.stop <- citibikes$end.station.name == "NULL"
-    citibikes <- citibikes[-which(null.start), ]
-    
-    # extract list of all stations and their locations
-    last <- as.character(unique(citibikes$start.station.name)[770]) # ...this one.
-    station.index <- unique(citibikes[, 8:11])
-    colnames(station.index) <- c("ID", "Name", "Latitude", "Longitude")
-    # add "last" to the station index
-    last.info <- unname(citibikes[citibikes$start.station.name == last, ][4:7])
-    colnames(last.info) <- colnames(station.index)
-    station.index <- rbind(station.index, last.info)
-    
-    station.index <- station.index[order(station.index$Name), ]
-    
-    ID <- as.character(station.index[, 1])
-    names <- as.character(station.index[, 2])
-    lat <- station.index[, 3]
-    long <- station.index[, 4]
-    
-    # display all bike stations on an interactive map
-    map <- leaflet() %>%
-      addTiles() %>%
-      addCircleMarkers(lng = long, lat = lat, 
-                       popup = paste(names, ", ID: ", ID, sep = ""),
-                       radius = 7)
-    map
-  })
+#  output$Netgraph <- renderLeaflet({
+#    # remove trips whose station ID's are null
+#    null.start <- citibikes$start.station.name == "NULL"
+#    null.stop <- citibikes$end.station.name == "NULL"
+#    citibikes <- citibikes[-which(null.start), ]
+#    
+#    # extract list of all stations and their locations
+#    last <- as.character(unique(citibikes$start.station.name)[770]) # ...this one.
+#    station.index <- unique(citibikes[, 8:11])
+#    colnames(station.index) <- c("ID", "Name", "Latitude", "Longitude")
+#    # add "last" to the station index
+#    last.info <- unname(citibikes[citibikes$start.station.name == last, ][4:7])
+#    colnames(last.info) <- colnames(station.index)
+#    station.index <- rbind(station.index, last.info)
+#    
+#    station.index <- station.index[order(station.index$Name), ]
+#    
+#    ID <- as.character(station.index[, 1])
+#    names <- as.character(station.index[, 2])
+#    lat <- station.index[, 3]
+#    long <- station.index[, 4]
+#    
+#    # display all bike stations on an interactive map
+#    map <- leaflet() %>%
+#      addTiles() %>%
+#      addCircleMarkers(lng = long, lat = lat, 
+#                       popup = paste(names, ", ID: ", ID, sep = ""),
+#                       radius = 7)
+#    map
+#  })
 })
